df30128e7757cee790169efa80ea56bb19216016
abanto <jobx10mx@gmail.com>
2024-08-15 12:45:14 +0800

    Set up Next.js deployment with GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/next-js-dep', // Replace with your GitHub repository name
+  assetPrefix: '/next-js-dep/', // Replace with your GitHub repository name
 };
 
 export default nextConfig;
