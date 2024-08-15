79d04ea0a8dfc09b84029b24691d0f27229fb489
jobx10 <jobx10mx@gmail.com>
2024-08-15 12:55:16 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   basePath: '/next-js-dep', // Replace with your GitHub repository name
   assetPrefix: '/next-js-dep/', // Replace with your GitHub repository name
-   output: 'export',  
+   output: 'export', 
+    trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
